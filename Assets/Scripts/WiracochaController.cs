e1048f49005f71d23059408a0e839a000f8ff374
Renzo Bendezu Pautrat <renzobptrt@gmail.com>
2019-10-12 17:40:18 -0500

    Añadio movimiento a Wiracocha

--- Assets/Scripts/WiracochaController.cs
+++ Assets/Scripts/WiracochaController.cs
@@ -5,37 +5,57 @@ using UnityEngine;
 public class WiracochaController : MonoBehaviour
 {   
 
-    public GameObject startPoint;
-    public GameObject endPoint;
+    private GameObject startPoint;
+    private GameObject endPoint;
+    private int numPoints;
+    private int indexInitial;
+    private int indexEnd;
+
+    public GameObject[] points;
 
     public float enemySpeed;
 
-    private bool isGoingRight;
+    private bool isDead;
     // Start is called before the first frame update
+
+    void Awake(){
+
+        this.indexEnd = 0;
+        this.numPoints = points.Length;
+        this.isDead = true;
+    }
+
     void Start()
-    {
-        if(isGoingRight){
+    {  
+        Debug.Log(numPoints);
+        /*if(isGoingRight){
             transform.position = startPoint.transform.position;
         }
         else{
             transform.position = endPoint.transform.position;
-        }
+        }*/
+        this.indexInitial = Random.Range(0,numPoints);
+        transform.position = points[indexInitial].transform.position;
+        indexEnd = RandomExcept(0,numPoints,indexInitial);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(!isGoingRight){
+    void FixedUpdate()
+    {   
+        if(isDead){
+            endPoint = points[indexEnd];
             transform.position = Vector3.MoveTowards(transform.position, endPoint.transform.position,enemySpeed * Time.deltaTime);
             if(transform.position == endPoint.transform.position){
-                isGoingRight = true;
+                indexEnd = RandomExcept(0,numPoints,indexEnd);
             }
         }
-        else{
-            transform.position = Vector3.MoveTowards(transform.position, startPoint.transform.position, enemySpeed * Time.deltaTime);
-            if(transform.position == startPoint.transform.position){
-                isGoingRight = false;
-            }
+    }
+
+    private int RandomExcept(int min,int max, int except){
+        int random = except;
+        while(random == except){
+            random = Random.Range(min,max);
         }
+        return random;
     }
 }
