32a379f958eaa62e8a79a18a11b89ab7d3adf54b
Renzo Bendezu Pautrat <renzobptrt@gmail.com>
2019-10-12 14:11:09 -0500

    Primer movimiento de Wiracocha

---
+++ Assets/Scripts/WiracochaController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiracochaController : MonoBehaviour
+{   
+
+    public GameObject startPoint;
+    public GameObject endPoint;
+
+    public float enemySpeed;
+
+    private bool isGoingRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isGoingRight){
+            transform.position = startPoint.transform.position;
+        }
+        else{
+            transform.position = endPoint.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isGoingRight){
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.transform.position,enemySpeed * Time.deltaTime);
+            if(transform.position == endPoint.transform.position){
+                isGoingRight = true;
+            }
+        }
+        else{
+            transform.position = Vector3.MoveTowards(transform.position, startPoint.transform.position, enemySpeed * Time.deltaTime);
+            if(transform.position == startPoint.transform.position){
+                isGoingRight = false;
+            }
+        }
+    }
+}
